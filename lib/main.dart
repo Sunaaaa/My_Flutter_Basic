2515376ddbc7b64e4aba58256aedfdfdb5b341fb
Sunaaaa <glglthssla@naver.com>
2021-02-09 16:19:51 +0900

    Layout 연습 (Stateless)

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,186 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// Custom 위젯 만들기
+Widget titleSection = new Container(
+  padding: const EdgeInsets.all(32),
+  decoration: new BoxDecoration(color: Colors.amber),
+  child: Row(
+    children: [
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: new Text(
+              'Hello :)',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          new Text(
+            'My name is Sun Ah!',
+            style: TextStyle(color: Colors.grey),
+          )
+        ],
+      )),
+      new Icon(
+        Icons.favorite_rounded,
+        color: Colors.pink,
+      ),
+      new Text('50')
+    ],
+  ),
+);
+Widget textSection = new Container(
+  padding: const EdgeInsets.all(32),
+  child: new Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
+
+
+int _count = 0;
+
+class LikeCount extends StatefulWidget{
+  @override
+  _LikeCount createState() => _LikeCount();
+}
+
+class _LikeCount extends State<LikeCount>{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text('$_count');
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        new Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+          // margin: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
+          child: new Text(
+            label,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.bold, color: color),
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      title: 'MyApp',
+      home: Scaffold(
+        appBar: AppBar(
+          title: new Text('Hi OMPANGI'),
+          backgroundColor: Colors.amber,
+          shadowColor: Colors.black,
+        ),
+        body: new ListView(
+          children: [
+            Image.asset(
+              'images/ompang.jpg',
+              width: 400,
+              height: 300,
+              fit: BoxFit.cover
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              padding: const EdgeInsets.all(32),
+              decoration: new BoxDecoration(color: Colors.amber[50]),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: new Text(
+                          'Hello :)',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      new Text(
+                        'My name is Sun Ah!',
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )),
+                  new Icon(
+                    Icons.favorite_rounded,
+                    color: Colors.pink,
+                  ),
+                  new Text('50')
+                ],
+              ),
             ),
+            Container(
+              padding: const EdgeInsets.only(top:20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildButtonColumn(Colors.teal[800], Icons.add_call, 'CALL'),
+                  _buildButtonColumn(Colors.teal[800], Icons.near_me, 'ROUTE'),
+                  _buildButtonColumn(Colors.teal[800], Icons.share, 'SHARE'),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(32),
+              child: new Text(
+                'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.',
+                softWrap: true,
+              ),
+            )
           ],
         ),
+        floatingActionButton: new FloatingActionButton(
+          // child: new Icon(Icons.add_a_photo),
+          child: new Icon(Icons.favorite_rounded, color: Colors.pink),
+          onPressed: () => {
+          },
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15.0))),
+          backgroundColor: Colors.green[900],
+        ),
+        bottomNavigationBar: new BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_alarm), title: new Text('Alarm')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.verified), title: new Text('Event')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.table_chart), title: new Text('Chart'))
+          ],
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.amber,
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
